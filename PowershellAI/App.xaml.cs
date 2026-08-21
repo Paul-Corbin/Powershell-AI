39261c4067251ec27dd273a7abbc87c670bf1a90
Paul Corbin <p.david.corbin@gmail.com>
2026-08-21 15:36:39 -0500

    Created tray icon for exit option

--- PowershellAI/App.xaml.cs
+++ PowershellAI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,5 +10,25 @@ namespace PowershellAI;
 /// </summary>
 public partial class App : Application
 {
-
+    private TaskbarIcon? Icon;
+    private void ExitClick(object sender, RoutedEventArgs e)
+    {
+        Shutdown();
+    }
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        Icon = (TaskbarIcon)FindResource("TrayIcon");
+    }
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (Icon == null) return;
+        Icon.Dispose();
+        base.OnExit(e);
+    }
+    private void ShowMenu(object sender, RoutedEventArgs e)
+    {
+        if (Icon == null) return;
+        Icon.ContextMenu.IsOpen = true;
+    }
 }
