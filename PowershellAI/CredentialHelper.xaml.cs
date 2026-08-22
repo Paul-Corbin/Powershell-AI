7dca5a92ea75c2f24d024705a640843ab67c32ff
Paul Corbin <p.david.corbin@gmail.com>
2026-08-21 20:59:38 -0500

    Wrote logic for CredentialHelper window which prompts for new API Keys

---
+++ PowershellAI/CredentialHelper.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace PowershellAI
+{
+    /// <summary>
+    /// Interaction logic for CredentialHelper.xaml
+    /// </summary>
+    internal partial class CredentialHelper : Window
+    {
+        internal CredentialHelper()
+        {
+            InitializeComponent();
+        }
+        private const string CredentialTarget = "PowershellAI_API_Key";
+
+        internal void StoreApiKey(string apiKey)
+        {
+            var credential = new CredentialManagement.Credential
+            {
+                Target = CredentialTarget,
+                Username = "APIKey",
+                Password = apiKey,
+                PersistanceType = CredentialManagement.PersistanceType.LocalComputer
+            };
+            credential.Save();
+        }
+        internal string GetApiKey()
+        {
+            try
+            {
+                var credential = new CredentialManagement.Credential
+                {
+                    Target = CredentialTarget
+                };
+                credential.Load();
+                return credential.Password;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        internal void RequestApiKey()
+        {
+            this.Show();
+        }
+    }
+}
